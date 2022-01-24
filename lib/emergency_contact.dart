3068960b66775bbe257d6262a1dd39fc68dfd4e7
AqeelSha7 <maqesha7@gmail.com>
2022-01-23 20:32:06 -0800

    7th commit

--- lib/emergency_contact.dart
+++ lib/emergency_contact.dart
@@ -1,4 +1,4 @@
-// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables, deprecated_member_use
+// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables, deprecated_member_use, avoid_unnecessary_containers, sized_box_for_whitespace
 
 import 'package:flutter/material.dart';
 
@@ -10,89 +10,234 @@ class EmergencyContact extends StatelessWidget {
     return Scaffold(
       backgroundColor: Color(0xFFDEDCD2),
       body: Center(
-        child: Column(children: <Widget>[
-          Container(
-            margin: EdgeInsets.only(
-                top: 60.0, left: 0.0, right: 170.0, bottom: 20.0),
-            child: Text(
-              "Emergency Contacts",
-              style: TextStyle(fontSize: 22.0),
-            ),
-          ),
-          Container(
-            height: 160.0,
-            width: 370.0,
-            decoration: BoxDecoration(
-              boxShadow: [
-                BoxShadow(
-                  color: Colors.white,
-                )
-              ],
-            ),
-            padding: EdgeInsets.fromLTRB(0, 10, 150, 10),
-           child: Column(
-            children: <Widget>[
-              
-             
-                 Text("FATHER     .",
-                style: TextStyle(fontSize: 22.0, letterSpacing: 2.0),),
-                Text("+94123456789 ",
-                style: TextStyle(fontSize: 22.0, letterSpacing: 2.0),),
-            ]
-          ),
-          ),
-          Container(
-            height: 53,
-            width: 334,
-            margin: EdgeInsets.only(
-                top: 25.0, left: 30.0, right: 30.0, bottom: 25.0),
-            child: RaisedButton(
-                onPressed: () {},
-              elevation: 0.0,
-              shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(30.0)),
-              child: Row(
-                mainAxisSize: MainAxisSize.min,
-                children: <Widget>[
-                  Image.asset(
-                    'images/alarm-warning-line.png',
-                    height: 100.0,
-                    width: 100.0,
-                  ),
-                  
-                  Padding(
-                      padding: EdgeInsets.fromLTRB(0, 1, 0, 0),
-                      child: Text(
-                        "Press To Call ",
-                        style: TextStyle(fontSize: 23.0),
-                      ))
-                ],
-                
+        child: Column(
+          children: <Widget>[
+            Container(
+              margin: EdgeInsets.only(
+                  top: 60.0, left: 0.0, right: 170.0, bottom: 20.0),
+              child: Text(
+                "Emergency Contacts",
+                style: TextStyle(fontSize: 22.0),
               ),
-               textColor: Colors.white,
-                    color: Color(0xFFE7301C)),
-              
-          
             ),
-          
-          Container(
+            Center(
+              child: Container(
+                 margin: EdgeInsets.only(
+                          top: 0.0, left: 15.0, right: 15.0, bottom: 5.0),
+                child: Card(
+                  child: Wrap(children: <Widget>[
+                    Padding(padding: EdgeInsets.only(top: 0,left: 10)),
+                    Text(
+                      "FATHER               ",
+                      style: TextStyle(fontSize: 22.0, letterSpacing: 2.0),
+                    ),
+                    Text(
+                      " +94123456789 ",
+                      style: TextStyle(fontSize: 22.0, letterSpacing: 2.0),
+                    ),
+                    Container(
+                      height: 53,
+                      width: 334,
+                      margin: EdgeInsets.only(
+                          top: 15.0, left: 30.0, right: 30.0, bottom: 25.0),
+                      child: RaisedButton(
+                          onPressed: () {},
+                          elevation: 0.0,
+                          shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(30.0)),
+                          child: Row(
+                            mainAxisSize: MainAxisSize.min,
+                            children: <Widget>[
+                              Image.asset(
+                                'images/alarm-warning-line.png',
+                                height: 100.0,
+                                width: 100.0,
+                              ),
+                              Padding(
+                                  padding: EdgeInsets.fromLTRB(0, 1, 0, 0),
+                                  child: Text(
+                                    "Press To Call ",
+                                    style: TextStyle(fontSize: 23.0),
+                                  ))
+                            ],
+                          ),
+                          textColor: Colors.white,
+                          color: Color(0xFFE7301C)),
+                    ),
+                  ]),
+                ),
+              ),
+              
+            ),
+            Center(
+              child: Container(
+                 margin: EdgeInsets.only(
+                          top: 0.0, left: 15.0, right: 15.0, bottom: 5.0),
+                child: Card(
+                  child: Wrap(children: <Widget>[
+                    Padding(padding: EdgeInsets.only(top: 0,left: 10)),
+                    Text(
+                      "HOSPITAL                        ",
+                      style: TextStyle(fontSize: 22.0, letterSpacing: 2.0),
+                    ),
+                    Text(
+                      " 1990 ",
+                      style: TextStyle(fontSize: 22.0, letterSpacing: 2.0),
+                    ),
+                    Container(
+                      height: 53,
+                      width: 334,
+                      margin: EdgeInsets.only(
+                          top: 15.0, left: 30.0, right: 30.0, bottom: 25.0),
+                      child: RaisedButton(
+                          onPressed: () {},
+                          elevation: 0.0,
+                          shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(30.0)),
+                          child: Row(
+                            mainAxisSize: MainAxisSize.min,
+                            children: <Widget>[
+                              Image.asset(
+                                'images/alarm-warning-line.png',
+                                height: 100.0,
+                                width: 100.0,
+                              ),
+                              Padding(
+                                  padding: EdgeInsets.fromLTRB(0, 1, 0, 0),
+                                  child: Text(
+                                    "Press To Call ",
+                                    style: TextStyle(fontSize: 23.0),
+                                  ))
+                            ],
+                          ),
+                          textColor: Colors.white,
+                          color: Color(0xFFE7301C)),
+                    ),
+                  ]),
+                ),
+              ),
+              
+            ),
+            Center(
+              child: Container(
+                 margin: EdgeInsets.only(
+                          top: 0.0, left: 15.0, right: 15.0, bottom: 0.0),
+                child: Card(
+                  child: Wrap(children: <Widget>[
+                    Padding(padding: EdgeInsets.only(top: 0,left: 10)),
+                    Text(
+                      "POLICE                              ",
+                      style: TextStyle(fontSize: 22.0, letterSpacing: 2.0),
+                    ),
+                    Text(
+                      " 119 ",
+                      style: TextStyle(fontSize: 22.0, letterSpacing: 2.0),
+                    ),
+                    Container(
+                      height: 53,
+                      width: 334,
+                      margin: EdgeInsets.only(
+                          top: 15.0, left: 30.0, right: 30.0, bottom: 25.0),
+                      child: RaisedButton(
+                          onPressed: () {},
+                          elevation: 0.0,
+                          shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(30.0)),
+                          child: Row(
+                            mainAxisSize: MainAxisSize.min,
+                            children: <Widget>[
+                              Image.asset(
+                                'images/alarm-warning-line.png',
+                                height: 100.0,
+                                width: 100.0,
+                              ),
+                              Padding(
+                                  padding: EdgeInsets.fromLTRB(0, 1, 0, 0),
+                                  child: Text(
+                                    "Press To Call ",
+                                    style: TextStyle(fontSize: 23.0),
+                                  ))
+                            ],
+                          ),
+                          textColor: Colors.white,
+                          color: Color(0xFFE7301C)),
+                    ),
+                  ]),
+                ),
+              ),
+              
+            ),
+              Container(
+              margin: EdgeInsets.only(
+                top: 10.0, left: 20.0, right: 20.0, bottom: 10.0),
+            height: 64,
+            width: 370,
+            child: ElevatedButton(
+              child: Text(
+                'Add Emergency Contact',
+                style: TextStyle(fontSize: 24.0),
+              ),
+             style: ButtonStyle(
+                backgroundColor:
+                    MaterialStateProperty.all<Color>(Color(0xFF014268)),
+                shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                    RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(25.0),
+                )),
+              ),
+              onPressed: () {
+               
+              },
+            ),
+          ),
+            Container(
+           margin: EdgeInsets.only(
+                top: 0.0, left: 20.0, right: 20.0, bottom: 10.0),
+            height: 64,
+            width: 370,
+            child: ElevatedButton(
+              child: Text(
+                'Remove Emergency Contact',
+                style: TextStyle(fontSize: 24.0),
+              ),
+              style: ButtonStyle(
+                backgroundColor:
+                    MaterialStateProperty.all<Color>(Color(0xFFE7301C)),
+                shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                    RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(25.0),
+                )),
+              ),
+              onPressed: () {
+            
+              },
+            ),
+          ),
+             Container(
             margin: EdgeInsets.only(
-                top: 55.0, left: 30.0, right: 30.0, bottom: 25.0),
-            height: 59,
-            width: 301,
+                top: 5.0, left: 30.0, right: 30.0, bottom: .0),
+            height: 60,
+            width: 194,
             child: ElevatedButton(
               child: Text(
                 'Back',
                 style: TextStyle(fontSize: 24.0),
               ),
-              style: ElevatedButton.styleFrom(
-                  primary: Color.fromRGBO(155, 154, 147, 1)),
+               style: ButtonStyle(
+                backgroundColor:
+                    MaterialStateProperty.all<Color>(Color(0xFF9B9A93)),
+                shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                    RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(25.0),
+                )),
+              ),
               onPressed: () {
                 Navigator.pop(context);
               },
             ),
           ),
-        ]),
+          ],
+        ),
       ),
     );
   }
