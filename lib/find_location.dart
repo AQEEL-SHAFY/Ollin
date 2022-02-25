932e33b5618a001718db9a0398ed18550492e1a0
UdeshikaMallawaarachchi <83880077+UdeshikaMallawaarachchi@users.noreply.github.com>
2022-02-26 00:25:22 +0530

    Added the map plugin

--- lib/find_location.dart
+++ lib/find_location.dart
@@ -3,6 +3,7 @@
 import 'package:flutter/material.dart';
 import 'package:ollin/emergency_contact.dart';
 import '';
+import 'google_map.dart';
 
 class FindLocation extends StatefulWidget {
   const FindLocation({Key? key}) : super(key: key);
@@ -18,14 +19,16 @@ class _FindLocationState extends State<FindLocation> {
     return Scaffold(
       body: Center(
         child: Column(children: <Widget>[
-          // Container(
-          //   child: GoogleMap(
-          //
-          //   ),
-          // ),
           Container(
             margin:
-            const EdgeInsets.only(top: 450.0, left: 50.0, right: 50.0),
+            const EdgeInsets.only(top: 50.0, left: 50.0, right: 50.0),
+            height: 500,
+            width: 309,
+            child: MapSample(),
+          ),
+          Container(
+            margin:
+            const EdgeInsets.only(top: 30.0, left: 50.0, right: 50.0),
             height: 72,
             width: 309,
             child: ElevatedButton(
@@ -46,37 +49,37 @@ class _FindLocationState extends State<FindLocation> {
               },
             ),
           ),
+          // Container(
+          //   margin:
+          //   const EdgeInsets.only(top: 20.0, left: 50.0, right: 50.0),
+          //   height: 72,
+          //   width: 309,
+          //   child: ElevatedButton(
+          //     child: Text(
+          //       'Emergency Contact',
+          //       style: TextStyle(fontSize: 24.0),
+          //     ),
+          //     style: ButtonStyle(
+          //       backgroundColor:
+          //       MaterialStateProperty.all<Color>(Color(0xFFE7301C)),
+          //       shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+          //           RoundedRectangleBorder(
+          //             borderRadius: BorderRadius.circular(25.0),
+          //           )),
+          //     ),
+          //     onPressed: () {
+          //       Navigator.push(
+          //         context,
+          //         MaterialPageRoute(
+          //             builder: (context) => const EmergencyContactWidget()),
+          //       );
+          //     },
+          //   ),
+          // ),
           Container(
             margin:
-            const EdgeInsets.only(top: 20.0, left: 50.0, right: 50.0),
+            const EdgeInsets.only(top: 10.0, left: 50.0, right: 50.0),
             height: 72,
-            width: 309,
-            child: ElevatedButton(
-              child: Text(
-                'Emergency Contact',
-                style: TextStyle(fontSize: 24.0),
-              ),
-              style: ButtonStyle(
-                backgroundColor:
-                MaterialStateProperty.all<Color>(Color(0xFFE7301C)),
-                shape: MaterialStateProperty.all<RoundedRectangleBorder>(
-                    RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(25.0),
-                    )),
-              ),
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                      builder: (context) => const EmergencyContactWidget()),
-                );
-              },
-            ),
-          ),
-          Container(
-            margin:
-            const EdgeInsets.only(top: 25.0, left: 50.0, right: 50.0),
-            height: 89,
             width: 309,
             child: ElevatedButton(
               child: Text(
