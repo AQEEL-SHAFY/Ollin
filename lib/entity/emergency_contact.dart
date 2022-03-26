177dd9b8628ad82461539f048109a2ed98df0d90
Sajee <thavaneswaran.20191055@iit.ac.lk>
2022-03-26 15:19:18 +0530

    Added Contacts and Working Database

--- lib/entity/emergency_contact.dart
+++
@@ -1,8 +0,0 @@
-class EmergencyContact{
-   int? id;
-  late String name;
-  late String number;
-
-   EmergencyContact(this.id, this.name, this.number);
-   EmergencyContact.newContact(this.name, this.number);
-}
--- lib/provider/smaple_prvider.dart
+++
@@ -1,3 +0,0 @@
-import 'package:flutter/cupertino.dart';
-
-class SampleProvider extends ChangeNotifier{}
